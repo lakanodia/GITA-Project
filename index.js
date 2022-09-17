d4f9bd291bcf41530ceee8e6c36a8a061a1849dc
Laka Nodia <laka.nodia@gmail.com>
2022-09-17 22:24:54 +0400

    Add second page

---
+++ index.js
@@ -0,0 +1,5 @@
+let dropdownList = document.getElementById('home-nav');
+let dropdownContent = document.getElementById('dropdown-content');
+dropdownList.addEventListener('click', function(){
+    dropdownContent.style.display = "block";
+})
